1ac033609c72fa9b1586ea973837a5b867046644
augusttex <augustowrcc@gmail.com>
2026-04-29 10:08:56 -0400

    inserção das funções get_time e somar_vetor

--- main.c
+++ main.c
@@ -8,3 +8,27 @@
 int vetor[SIZE];
 long long soma_parcial[NUM_THREADS];
 
+double get_time() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec + tv.tv_usec / 1000000.0;
+}
+
+void* somar_vetor(void* arg) {
+    int id = *((int*)arg);
+    
+    int tamanho_fatia = SIZE / NUM_THREADS;
+    int inicio = id * tamanho_fatia;
+    int fim = (id == NUM_THREADS - 1) ? SIZE : inicio + tamanho_fatia;
+
+    long long soma_local = 0;
+
+    for (int i = inicio; i < fim; i++) {
+        soma_local += (vetor[i] * 5) / 5;
+    }
+
+    soma_parcial[id] = soma_local;
+
+    return NULL;
+}
+
